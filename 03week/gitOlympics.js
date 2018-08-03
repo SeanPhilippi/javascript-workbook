cbe116ea2884ee31fa08079435b6f6320d60b248
SeanPhilippi <sean.philippi@gmail.com>
2018-08-02 19:18:58 -0500

    basic idea

---
+++ 03week/gitOlympics.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let nameArr = [
+  'Sean', 'Rob', 'Bob', 'Peter', 'John'
+]
+
+const printListOfOlympians = (nameArr) => {
+  nameArr.forEach(function(){
+    console.log()
+  })
+}
+
+printListOfOlympians();
