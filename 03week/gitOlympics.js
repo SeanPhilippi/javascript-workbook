b625d2c884e42ba1a10a2bd40717414b60ca442e
SeanPhilippi <sean.philippi@gmail.com>
2018-08-02 20:01:17 -0500

    still needs some work

--- 03week/gitOlympics.js
+++ 03week/gitOlympics.js
@@ -1,30 +1,11 @@
-// 'use strict'
-//
-// let nameArr = [
-//   'Sean', 'Rob', 'Bob', 'Peter', 'John'
-// ]
-//
-// const printListOfOlympians = (nameArr) => {
-//   nameArr.forEach(function(){
-//     console.log()
-//   })
-// }
-//
-// printListOfOlympians();
-
-// some comments added
-// more comments added
 'use strict';
 
-let nameArr = [
-  'Sean', 'Rob', 'Bob', 'Peter', 'John'
-]
-function printListOfOlympians(nameArr){
+let nameArr = ['Sean', 'Rob', 'Bob', 'Peter', 'John'];
+const printListOfOlympians=(nameArr)=>{
 nameArr.forEach(function(entry){
 console.log(nameArr);
 });
 
 }
 
-printListOfOlympians([
-'Sean', 'Rob', 'Bob', 'Peter', 'John'])
+printListOfOlympians(nameArr);
